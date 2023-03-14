30437a88e43da410f6c9a4266b513c0677c20336
AlexFreezeman <winterirk@gmail.com>
2023-03-15 00:04:54 +0800

    Initial commit

---
+++ el_shop/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from utils.item import Item
+
+
+@pytest.fixture()
+def keyboard():
+    keyboard = Item("клавиатура", 2400, 5)
+
+    return keyboard
---
+++ utils/item.py
@@ -0,0 +1,19 @@
+class Item:
+    pay_rate = 1.0
+
+    def __init__(self, name="", price=0.0, quantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __repr__(self) -> str:
+        text = ""
+        for dic in self.__dict__:
+            text += dic + "=" + str(self.__dict__[dic]) + ", "
+        return f"Goods({text[:-2]})"
+
+    def calculate_amount(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
