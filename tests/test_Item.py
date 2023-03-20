ef0c4bd21d42d3fe01b7a5bce62b853e0556e7cf
AlexFreezeman <winterirk@gmail.com>
2023-03-20 22:01:09 +0800

    task 1 fix 2.5 idk

--- tests/test_Item.py
+++ tests/test_Item.py
@@ -11,7 +11,7 @@ def test_apply_discount(keyboard):
     assert keyboard.pay_rate == 1
     item1 = Item("name", 50, 2)
     item1.pay_rate = 0.8
-    assert item1.apply_discount() == None
+    assert item1.apply_discount() is None
     assert item1.price == 40
     assert int(item1.calculate_amount()) == 80
 
