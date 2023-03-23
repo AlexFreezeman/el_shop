d364dd9e44224aee515a452969a86df95963a684
AlexFreezeman <winterirk@gmail.com>
2023-03-23 20:28:29 +0800

    task 2 (upd 1 -- tests)

--- tests/test_Item.py
+++ tests/test_Item.py
@@ -1,14 +1,6 @@
 import pytest
-import pytest_cov
 from utils.item import Item
-
-
-def test__init__(keyboard):
-    assert keyboard._Item__name == "клавиатура"
-    assert keyboard.price == 2400
-    assert keyboard.quantity == 5
-    with pytest.raises(NameError, match="Длина названия товара не должна превышать 10 символов!"):
-        item = Item("очень длинное имя", 1, 1)
+from utils.config import path_csv
 
 
 def test_calculate_amount(keyboard):
@@ -21,24 +13,35 @@ def test_apply_discount(keyboard):
     assert keyboard.pay_rate == 1
     item1 = Item("name", 50, 2)
     item1.pay_rate = 0.8
-    assert item1.apply_discount() == 40.0
+    assert item1.apply_discount() is None
     assert item1.price == 40
     assert int(item1.calculate_amount()) == 80
 
 
+def test_all_items():
+    item1 = Item("test_1", 10_000, 20)
+    item2 = Item("test_2", 20_000, 5)
+
+    assert Item.all == [item1, item2]
+
+
+def test_name():
+    item3 = Item("СуперВещь9999", 50000, 1)
+    with pytest.raises(Exception):
+        item3.name = "Длина названия товара превышает 10 символов."
+
+
 def test_is_integer():
-    item = Item("name", 50, 2)
+    item = Item("name", 1000, 5)
     assert item.is_integer(5) is True
     assert item.is_integer(5.0) is True
     assert item.is_integer(5.5) is False
     assert item.is_integer("5") is False
 
 
-def test__repr__(keyboard):
-    assert str(keyboard) == "Item(_Item__name=клавиатура, price=2400, quantity=5)"
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv("items.csv")
+    assert len(Item.all) == 5
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[4].name == "Клавиатура"
 
-
-# def test_load_from_csv(path_csv_file) -> list:
-#    item = Item("name", 50, 2)
-#    assert len(item.load_from_csv(path_csv_file)) == 5
-#    assert isinstance(item.load_from_csv(path_csv_file)[0], Item)
